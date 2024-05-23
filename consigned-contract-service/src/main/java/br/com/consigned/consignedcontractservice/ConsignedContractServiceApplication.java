60b1ca461e5687032efa74bd45310cd9c1a20ddf
EmilyCV <emily_cvasconcelos@hotmail.com>
2024-05-23 01:23:55 -0300

    Adjustment to the base structure

---
+++ consigned-contract-service/src/main/java/br/com/consigned/consignedcontractservice/ConsignedContractServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.consigned.consignedcontractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsignedContractServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsignedContractServiceApplication.class, args);
+	}
+
+}
---
+++ user-verification-service/src/main/java/br/com/consigned/userverificationservice/UserVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.consigned.userverificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVerificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserVerificationServiceApplication.class, args);
+	}
+
+}
