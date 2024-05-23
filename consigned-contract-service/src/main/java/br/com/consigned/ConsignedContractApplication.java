890fd212239edbeb8948b587d9dffd4e6ed3fffd
EmilyCV <emily_cvasconcelos@hotmail.com>
2024-05-23 00:07:07 -0300

    Initial commit basic structure

---
+++ consigned-contract-service/src/main/java/br/com/consigned/ConsignedContractApplication.java
@@ -0,0 +1,11 @@
+package br.com.consigned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsignedContractApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsignedContractApplication.class, args);
+    }
+}
