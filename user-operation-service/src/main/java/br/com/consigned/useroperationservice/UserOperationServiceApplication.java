fb8d89e0255e545908a7c765123c44a01d3a7d0b
EmilyCV <emily_cvasconcelos@hotmail.com>
2024-05-26 17:02:10 -0300

    project basis and defined endpoints

---
+++ user-operation-service/src/main/java/br/com/consigned/useroperationservice/UserOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.consigned.useroperationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOperationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOperationServiceApplication.class, args);
+	}
+
+}
---
+++ user-operation-service/src/test/java/br/com/consigned/useroperationservice/UserOperationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consigned.useroperationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOperationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
