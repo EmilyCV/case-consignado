6947f1a562635c4d2a46595f9ea3986dd6ef5931
EmilyCV <emily_cvasconcelos@hotmail.com>
2024-05-31 19:47:13 -0300

    hash of the document added to the consigned model module

---
+++ consigned-model/src/main/java/br/com/consigned/consigned_model/util/HashConverter.java
@@ -0,0 +1,19 @@
+package br.com.consigned.consigned_model.util;
+
+import jakarta.xml.bind.DatatypeConverter;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashConverter {
+    public static String getHash(String document) throws Exception {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(document.getBytes());
+            byte[] digest = md.digest();
+            return DatatypeConverter.printHexBinary(digest).toLowerCase();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating hash: " + e.getMessage());
+        }
+    }
+}
