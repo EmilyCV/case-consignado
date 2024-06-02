1c379878c170954176757e54d0e79415f3f8b102
EmilyCV <emily_cvasconcelos@hotmail.com>
2024-06-02 18:53:46 -0300

    log adjustment

--- consigned-simulator-service/src/main/java/br/com/consigned/consignedsimulatorservice/kafka/consumer/SimulationConsumer.java
+++ consigned-simulator-service/src/main/java/br/com/consigned/consignedsimulatorservice/kafka/consumer/SimulationConsumer.java
@@ -3,16 +3,15 @@ package br.com.consigned.consignedsimulatorservice.kafka.consumer;
 import br.com.consigned.consignedsimulatorservice.model.SimulationRegistration;
 import br.com.consigned.consignedsimulatorservice.service.SimulationRegistrationService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
+@Slf4j
 @Component
 public class SimulationConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(SimulationConsumer.class);
     private final SimulationRegistrationService simulationRegistrationService;
 
     @KafkaListener(topics = "${topic.simulation.consumer.result-simulation}", groupId = "${topic.simulation.consumer.group-id}")
